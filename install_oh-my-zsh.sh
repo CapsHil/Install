99ceb0263868243bf260d9767573908008e681e7
Pierre <pierre.raby.pro@gmail.com>
2015-06-02 10:46:57 +0200

    Update omz install

--- install_oh-my-zsh.sh
+++ install_oh-my-zsh.sh
@@ -1,3 +1,3 @@
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 cp oh-my-zsh/.zshrc ~/
-cp -r oh-my-zsh/.oh-my-zsh ~/
+cp -r oh-my-zsh/.oh-my-zsh/custom ~/.oh-my-zsh/
