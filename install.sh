f55e643f000755e4d981a8df258e79cdcd673b41
CapsHil <moi.pierre.raby@gmail.com>
2016-01-16 17:11:55 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+#
+# Installation script
+# Install zsh, oh-my-zsh, Apache Server and dependencies, git
+# 
+
+sudo su
+apt-get update
+apt-get upgrade
+apt-get install git wget vim apache2 php5 mysql-server libapache2-mod-php5 php5-mysql phpmyadmin zsh
+chsh -s /bin/zsh
